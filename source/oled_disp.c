5c9a7ae5f32d665334aef9052f29939c3be01b95
Jason Yeh <jjyeh1976@gmail.com>
2022-01-19 08:25:03 +0800

    first commit

---
+++ source/oled_disp.c
@@ -0,0 +1,48 @@
+#include "ch.h"
+#include "hal.h"
+#include "ssd1362.h"
+#include "oled_disp.h"
+
+#include "ssd1306_font.c"
+
+static const I2CConfig i2ccfg = {
+  OPMODE_I2C,
+  100000,
+  STD_DUTY_CYCLE,
+};
+
+static const SSD1362Config config = {
+  &I2CD1,
+  &i2ccfg,
+  SSD1362_SAD_0X78,
+};
+
+static SSD1362Driver ssd1362;
+
+void oled_disp_init()
+{
+  ssd1362ObjectInit(&ssd1362);
+  
+  palClearPad(GPIOB,9);
+  chThdSleepMilliseconds(10);
+  palSetPad(GPIOB,9);
+  
+  ssd1362Start(&ssd1362,&config);
+  
+  ssd1362FillScreen(&ssd1362,0x0);
+  
+  char msg[32];
+  
+  sprintf(msg,"Test of Grididea.com.tw\0");
+  ssd1362GotoXy(&ssd1362,0,0);
+  ssd1362Puts(&ssd1362,msg,&ssd1306_font_11x18,0xc);
+
+  ssd1362GotoXy(&ssd1362,0,20);
+  ssd1362Puts(&ssd1362,msg,&ssd1306_font_11x18,0xc);
+
+  ssd1362GotoXy(&ssd1362,0,40);
+  ssd1362Puts(&ssd1362,msg,&ssd1306_font_11x18,0xc);
+
+
+  ssd1362UpdateScreen(&ssd1362);
+}
---
+++ source/oled_disp.h
@@ -0,0 +1,4 @@
+#ifndef _OLED_DISP_
+#define _OLED_DISP_
+
+#endif
